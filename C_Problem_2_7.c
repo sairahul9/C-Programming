f56423621834dc6d0862784a229ea096d396eea9
Rahul <sairahul745@gmail.com>
2025-12-07 22:20:50 +0530

    level 2 prob 7 and 8

---
+++ C_Problem_2_7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,u,t;
+    printf("Enter a Number:");
+    scanf("%d",&n);
+    u = n%10;
+    t = n/10;
+    if (u == t){
+        printf("Result = 1");
+    }
+    else{
+        printf("Result = 0");
+    }
+    
+}
